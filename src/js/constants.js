7665a5440a3740cb61f98c8a39ed307860427d72
blackmumba <ok.semonenko@gmail.com>
2018-08-30 23:42:23 +0300

    add constants list

---
+++ src/js/constants.js
@@ -0,0 +1,16 @@
+export const Result = {
+    running: 'running',
+    win: 'win',
+    draw: 'draw'
+};
+
+export const CellValue = {
+    X: 'X',
+    O: 'O',
+    Empty: 'Empty'
+};
+
+export const Level = {
+    easy: 'easy',
+    hard: 'hard'
+};

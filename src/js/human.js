bbb8913963e05cdd95e31dc6673db5339b8805e4
blackmumba <ok.semonenko@gmail.com>
2018-08-31 21:39:30 +0300

    add Human implementation

--- src/js/human.js
+++ src/js/human.js
@@ -1,21 +1,57 @@
-export default function () {
+import AI from './AI';
+import Game from './Game';
+import {Level} from "./constants";
+
+export default function Human () {
+    console.log('human');
     let globals = {};
 
-    this.onCellClick = () => {
+    function onCellClick() {
+        console.log('cell clicked');
+    }
 
-    };
+    function onLevelBtnClick(event) {
+        const clickedLevelBtn = event.target;
+        const selected = document.querySelector('.selected');
+        const level = event.target.getAttribute('id');
+        if (!selected) {
+            clickedLevelBtn.classList.add('selected');
+        }
+        else {
+            selected.classList.toggle('selected');
+            clickedLevelBtn.classList.add('selected');
+        }
+        console.log('level choose', level);
+    }
 
-    this.onStartBtnClick = () => {
+    function onStartBtnClick() {
+        const selectedLevel = document.querySelector('.selected')
+            ? document.querySelector('.selected').getAttribute('id')
+            : Level.easy;
 
-    };
+        if (!selectedLevel) {
+            return;
+        }
+        const aiPlayer = new AI(selectedLevel);
+        globals.game = new Game(aiPlayer);
+        aiPlayer.plays(globals.game);
+        globals.game.start();
+        console.log('start clicked', selectedLevel);
+    }
+
+    let levels = document.querySelectorAll('.level');
+
+    levels.forEach(level => {
+        level.addEventListener('click', onLevelBtnClick);
+    });
 
     let startBtn = document.querySelector('.start');
 
-    startBtn.addEventListener('onclick', this.onStartBtnClick);
+    startBtn.addEventListener('click', onStartBtnClick);
 
     let cells = document.querySelectorAll('.cell');
 
     cells.forEach(cell => {
-        cell.addEventListener('onclick', this.onCellClick)
+        cell.addEventListener('click', onCellClick)
     })
 };

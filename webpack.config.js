18310057684010b55a41412b801eb9d796bc0688
blackmumba <ok.semonenko@gmail.com>
2018-08-31 15:13:01 +0300

    add webpack config, dependencies in package.json and change path in html

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const argv = require('yargs').argv;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = argv.mode === 'development';
+const isProduction = !isDevelopment;
+const distPath = path.join(__dirname, '/public');
+
+const config = {
+    entry: {
+        main: './src/js/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: distPath
+    },
+    module: {
+        rules: [{
+            test: /\.html$/,
+            use: 'html-loader'
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }]
+        }, {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [
+                isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        minimize: isProduction
+                    }
+                },
+                'sass-loader',
+                'resolve-url-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    optimization: isProduction ? {
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    compress: {
+                        inline: false,
+                        warnings: false,
+                        drop_console: true,
+                        unsafe: true
+                    },
+                },
+            }),
+        ],
+    } : {},
+    devServer: {
+        contentBase: distPath,
+        port: 8081,
+        compress: true,
+        open: true
+    }
+};
+module.exports = config;

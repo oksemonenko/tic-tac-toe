5679bf3122ee898282d27dedd95b1b3e61b2a90f
blackmumba <ok.semonenko@gmail.com>
2018-09-01 03:09:39 +0300

    implements onCellClick listener

--- src/js/human.js
+++ src/js/human.js
@@ -1,32 +1,51 @@
 import AI from './AI';
 import Game from './Game';
-import {Level} from "./constants";
+import State from './State';
+import {
+    CellValue,
+    Level,
+    Status
+} from "./constants";
 
 export default function Human () {
     console.log('human');
     let globals = {};
 
-    function onCellClick() {
+    function onCellClick(event) {
+        const cell = event.target;
+        const occupiedCellClassName = 'board__cell--occupied';
+        if (
+            globals.game.status === Status.running
+            && globals.game.currentState.turn === CellValue.X
+            && cell.classList.contains(occupiedCellClassName)
+        ) {
+            const index = Number(cell.getAttribute('id'));
+
+            const nextState = new State(globals.game.currentState);
+            nextState.board[index] = CellValue.X;
+        }
         console.log('cell clicked');
     }
 
     function onLevelBtnClick(event) {
+        const selectedLevelClassName = 'difficulty__level--selected';
         const clickedLevelBtn = event.target;
-        const selected = document.querySelector('.selected');
+        const selected = document.querySelector('.' + selectedLevelClassName);
         const level = event.target.getAttribute('id');
         if (!selected) {
-            clickedLevelBtn.classList.add('selected');
+            clickedLevelBtn.classList.add(selectedLevelClassName);
         }
         else {
-            selected.classList.toggle('selected');
-            clickedLevelBtn.classList.add('selected');
+            selected.classList.toggle(selectedLevelClassName);
+            clickedLevelBtn.classList.add(selectedLevelClassName);
         }
         console.log('level choose', level);
     }
 
     function onStartBtnClick() {
-        const selectedLevel = document.querySelector('.selected')
-            ? document.querySelector('.selected').getAttribute('id')
+        const selectedLevelClassName = 'difficulty__level--selected';
+        const selectedLevel = document.querySelector('.' + selectedLevelClassName)
+            ? document.querySelector('.' + selectedLevelClassName).getAttribute('id')
             : Level.easy;
 
         if (!selectedLevel) {
@@ -36,20 +55,21 @@ export default function Human () {
         globals.game = new Game(aiPlayer);
         aiPlayer.plays(globals.game);
         globals.game.start();
+
         console.log('start clicked', selectedLevel);
     }
 
-    let levels = document.querySelectorAll('.level');
+    let levels = document.querySelectorAll('.difficulty__level');
 
     levels.forEach(level => {
         level.addEventListener('click', onLevelBtnClick);
     });
 
-    let startBtn = document.querySelector('.start');
+    let startBtn = document.querySelector('.start-btn');
 
     startBtn.addEventListener('click', onStartBtnClick);
 
-    let cells = document.querySelectorAll('.cell');
+    let cells = document.querySelectorAll('.board__cell');
 
     cells.forEach(cell => {
         cell.addEventListener('click', onCellClick)
